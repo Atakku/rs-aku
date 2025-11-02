b09fc37298487231e68141acdeabd80cda10280a
Atakku <atakkudev@gmail.com>
2025-11-02 16:31:52 +0100

    fix

--- modules/postgres/src/schema.rs
+++ modules/postgres/src/schema.rs
@@ -38,7 +38,7 @@ macro_rules! schema {
       $(.col(sea_query::ColumnDef::new($table::$col)$(.$($col_tt)*)*)
       )*
       .build(sea_query::PostgresQueryBuilder), sqlx::postgres::PgArguments::default())
-      .execute($crate::get_pg()?).await?;
+      .execute($crate::pg()?).await?;
       )*
       Ok(())
     }
